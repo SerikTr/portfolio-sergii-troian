f461f62a22f15f023dd3576b8cb2396fa25d9b6e
SerikTr <seriktr@gmail.com>
2021-02-05 21:28:59 +0100

    made sketches with a home page. Worked out the menu and responsive design for header.

--- src/app/shared/main-layout/main-layout.component.ts
+++ src/app/shared/main-layout/main-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-main-layout',
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainLayoutComponent implements OnInit {
 
-  constructor() { }
+  status: boolean = true
+
+  // TODO поработать над динамическим выводом меню
+  // TODO при выборе страницы в  меню  бургере, страница переключается но меню не прячется
+  // TODO можно начинать следующую страницу About
+  // https://sergii-troian-portfolio.web.app/about.html
+  // navigation: [
+  //   {routerHome: '/', name: 'Home', id?},
+  //   {routerAbout: '/about', name: 'About Me', id?},
+  //   {routerWork: '/work', name: 'My Work', id: 1},
+  //   {routerContact: '/contact', name: 'How To Reach Me', id?}
+  //   ]
+
 
   ngOnInit(): void {
+
   }
 
+
+
 }
+
+
+
+
